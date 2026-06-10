9c25bb91002f31676737f7079ca569dfdedc587c
mac <@b.tunde.ipaye@gmail.com>
2026-06-10 20:58:44 +0100

    feat: dependencies updated commit by ENGR. IPAYE

---
+++ src/layout/Navbar.jsx
@@ -0,0 +1,13 @@
+export const Navbar = () => {
+    return (
+        <header>
+            <nav>
+                <a>
+                    PM<span></span>
+                </a>
+            </nav>
+
+
+        </header>
+    );
+};
