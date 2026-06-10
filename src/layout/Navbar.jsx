deb05afe675b0aea05a80513f680eb484d7a46af
mac <@b.tunde.ipaye@gmail.com>
2026-06-11 00:51:05 +0100

    feat: react code updated commit by ENGR. IPAYE

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -11,6 +11,7 @@ export const Navbar = () => {
         <header className="fixed top-0 left-0 right-0 bg-transparent py-5">
             <nav className="container mx-auto px-6 flex items-center justify-between">
                 <a>
+
                     PM<span>.</span>
                 </a>
 
