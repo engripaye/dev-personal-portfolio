9e84196cb0142864b13d31905e722680013edc83
mac <@b.tunde.ipaye@gmail.com>
2026-06-11 01:09:00 +0100

    feat: react code updated commit by ENGR. IPAYE

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -18,7 +18,8 @@ export const Navbar = () => {
                 <div className="flex items-center gap-1">
                     <div className="glass rounded-full px-2 py-1 flex items-center gap-1">
                         {navLinks.map((link, index) =>(
-                            <a href={link.href} key={index}>{link.label}</a>
+                            <a href={link.href} key={index} className="px-4 py-2 text-sm text-muted-foreground hover:text-foreground rounded-full">
+                                {link.label}</a>
                         ))}
                     </div>
                 </div>
