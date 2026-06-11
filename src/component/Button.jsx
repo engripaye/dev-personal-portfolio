7523433a94a6bd4fa4c5ff569313e82df59c930f
mac <@b.tunde.ipaye@gmail.com>
2026-06-11 11:30:22 +0100

    feat: react code updated commit by ENGR. IPAYE

--- src/component/Button.jsx
+++ src/component/Button.jsx
@@ -1,6 +1,6 @@
 
 
-export const Button = ({ className, size ="default", children }) => {
+export const Button = ({ className= "", size ="default", children }) => {
 
     const baseClasses = "relative overflow-hidden rounded-full font-medium focus:outline-none focus-visible:ring-2 focus-visible:ring-primary bg-primary text-primary-foreground hover:bg-primary/90 shadow-lg shadow-primary/25";
 
@@ -9,7 +9,7 @@ export const Button = ({ className, size ="default", children }) => {
         default: "px-6 py-3 text-base",
         lg: "px-8 py-4 text-lg",
     }
-    const classes = `${baseClasses} ${sizeClasses[size]}`;
+    const classes = `${baseClasses} ${sizeClasses[size]} ${className}`;
     return (
 
         <button className={classes}>
