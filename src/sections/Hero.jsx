2fe2c640821c92188418f618de407b3eb87ba829
mac <@b.tunde.ipaye@gmail.com>
2026-06-11 17:02:30 +0100

    feat: react code updated commit by ENGR. IPAYE

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,3 +1,12 @@
 export const Hero = () => {
-    return <section className="relative min-h-screen flex items-center overflow-hidden"></section>
+    return (
+        <section className="relative min-h-screen flex items-center overflow-hidden">
+
+    {/* Bg */}
+            <div>
+                <img src="/hero.jpg" />
+            </div>
+
+        </section>
+    )
 }
