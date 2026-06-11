ac3323099841ff9b22c93e6a11c58cf8b153a054
mac <@b.tunde.ipaye@gmail.com>
2026-06-11 14:45:02 +0100

    feat: react code updated commit by ENGR. IPAYE

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,5 +1,6 @@
 import { Button } from "../component/Button";
 import { Menu } from "lucide-react";
+import { useState } from "react";
 const navLinks = [
     {href: "#about", label: "About"},
     {href: "#projects", label: "Projects"},
@@ -8,6 +9,8 @@ const navLinks = [
 ];
 
 export const Navbar = () => {
+
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
     return (
         <header className="fixed top-0 left-0 right-0 bg-transparent py-5">
             <nav className="container mx-auto px-6 flex items-center justify-between">
@@ -30,25 +33,28 @@ export const Navbar = () => {
                 </div>
 
                 {/* Mobile Menu Button */}
-                <button className="md:hidden p-2 text-foreground">
+                <button className="md:hidden p-2 text-foreground" onClick={()=>setIsMobileMenuOpen((prev) => !prev)}>
                     <Menu size={24} />
                 </button>
             </nav>
 
             {/* Mobile Menu */}
-            <div className="md:hidden glass-strong">
-                <div className="container mx-auto px-6 py-6 flex flex-col gap-4 ">
+            {isMobileMenuOpen && (
+                <div className="md:hidden glass-strong">
+                <div className="container mx-auto px-6 py-6 flex flex-col gap-4">
                     {navLinks.map((link, index) =>(
                         <a href={link.href}
                            key={index}
                            className="text-lg text-muted-foreground hover:text-foreground py-2">
-                            {link.label}</a>
+                            {link.label}
+                        </a>
                     ))}
 
                     <Button>Contact Me</Button>
                 </div>
 
             </div>
+            )}
 
         </header>
     );
