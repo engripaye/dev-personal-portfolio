9f011ea06577feb7cf78c3ba1c87836babd6642b
mac <@b.tunde.ipaye@gmail.com>
2026-06-11 17:09:54 +0100

    feat: react code updated commit by ENGR. IPAYE

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -7,7 +7,9 @@ export const Hero = () => {
                 <img
                      src="/hero-bg.jpg"
                      alt="Hero image"
-                     className="w-full h-full object-cover opacity-40"/>
+                     className="w-full h-full object-cover opacity-40"
+                />
+                <div className="absolute inset-0 bg-gradient-to-b from-background/20 via-background/80 to-background" />
             </div>
 
         </section>
