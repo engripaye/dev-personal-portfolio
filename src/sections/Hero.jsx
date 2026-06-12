1dcfd36114db45acb82009514e0c6b4444136ac3
mac <@b.tunde.ipaye@gmail.com>
2026-06-12 18:37:35 +0100

    feat: react code updated commit by ENGR. IPAYE

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -115,7 +115,7 @@ export const Hero = () => {
                                 </div>
                                 {/* Stats Badge*/}
                                 <div className="absolute -top-4 -left-4 glass rounded-xl px-4 py-3 animate-float animation-delay-500">
-                                    <div className="">5+</div>
+                                    <div className=>5+</div>
                                     <div>Years Exp.</div>
 
                                 </div>
