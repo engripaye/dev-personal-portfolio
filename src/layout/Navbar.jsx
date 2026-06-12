3e53501363c17bdb131ea4450516f7a35959579a
mac <@b.tunde.ipaye@gmail.com>
2026-06-12 21:36:28 +0100

    feat: react code updated commit by ENGR. IPAYE

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -15,10 +15,10 @@ export const Navbar = () => {
 
     useEffect(() =>{
         const handleScroll = () => {
-
+            setIsScrolled(window.scrollY > 50);
         }
-
-        window.addEventListener("scroll", )
+        window.addEventListener("scroll",handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
     }, []);
 
     return (
