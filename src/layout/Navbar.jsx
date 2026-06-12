d30254b46b97634579c52ac2a832edf15e414e3e
mac <@b.tunde.ipaye@gmail.com>
2026-06-12 21:38:12 +0100

    feat: react code updated commit by ENGR. IPAYE

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -22,7 +22,7 @@ export const Navbar = () => {
     }, []);
 
     return (
-        <header className="fixed top-0 left-0 right-0 bg-transparent py-5 z-50">
+        <header className={`fixed top-0 left-0 right-0 ${isScrolled ? "" : ""} bg-transparent py-5 z-50`}>
             <nav className="container mx-auto px-6 flex items-center justify-between">
                 <a href="#" className="text-xl font-bold tracking-tight hover:text-primary">
                     ENGR. IPAYE<span className="text-primary">.</span>
