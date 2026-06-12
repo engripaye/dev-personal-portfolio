4b5a86bb8054a2a4b2284483e3d5fed7fee2db0d
mac <@b.tunde.ipaye@gmail.com>
2026-06-12 17:53:55 +0100

    feat: react code updated commit by ENGR. IPAYE

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -94,7 +94,7 @@ export const Hero = () => {
                     <div className="relative animate-fade-in animation-delay-300">
                         {/* Profile Image */}
                         <div className="relative max-w-md mx-auto">
-                            <div>
+                            <div className="relative glass rounded-3xl p-2 glow-border">
                                 <img src="/pro1.png"
                                      alt="Engr. Ipaye"
                                      className="w-full aspect-[4/5] object-cover rounded-2xl"/>
