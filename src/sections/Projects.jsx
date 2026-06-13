c4fbb54def696c752fe7fafc6362a3a0fa87cef6
mac <@b.tunde.ipaye@gmail.com>
2026-06-13 16:24:44 +0100

    feat: react code updated commit by ENGR. IPAYE

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -134,12 +134,12 @@ export const Projects = () => {
                                     to-transparent opacity-60"
                                 />
                                 {/* Overlay Links */}
-                                <div>
-                                    <a>
+                                <div className="absolute inset-0 flex items-center justify-center gap-4 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                                    <a href={projects.link} className="p-3 rounded-full glass hover:bg-primary hover:text-primary-foreground">
                                         <ArrowUpRight />
                                     </a>
                                     <a>
-                                        <Github />
+                                        <FaGithub />
                                     </a>
                                 </div>
                             </div>
