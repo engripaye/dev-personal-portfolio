23f8554cc8c2fc2940de53f8353083a61eec602e
mac <@b.tunde.ipaye@gmail.com>
2026-06-13 15:30:19 +0100

    feat: react code updated commit by ENGR. IPAYE

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -83,5 +83,23 @@ const projects = [
 ]
 
 export const Projects = () => {
-    return <section></section>
-}
+    return (
+
+        <section id="projects" className="py-32 relative overflow-hidden">
+            {/* Bg glows */}
+            <div className="absolute top-1/4 right-0 w-96 h-96 bg-primary/5 rounded-full blur-3xl" />
+            <div className="absolute bottom-1/4 left-0 w-64 h-64 bg-highlight/5 rounded-full blur-3xl" />
+            <div className="container mx-auto px-6 relative z-10"></div>
+
+            {/* Section Header */}
+            <div>
+                <span>Featured Work</span>
+                <h2>
+                    Projects that
+                    <span> make an impact. </span>
+                </h2>
+            </div>
+        </section>
+
+    );
+};
