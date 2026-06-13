c4a9c0552c5b87b05ee2be12e8e93081e795564a
mac <@b.tunde.ipaye@gmail.com>
2026-06-13 18:04:03 +0100

    feat: react code updated commit by ENGR. IPAYE

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -151,8 +151,8 @@ export const Experience = () => {
                             <div></div>
 
                             {/* Content */}
-                            <div>
-                                <div>
+                            <div className={`pl-8 md:pl-0 ${idx % 2 === 0 ? "md:pr-16 md:text-right" : "md:col-start-2 md:pl-16"}`}>
+                                <div className={`glass p-6 rounded-2xl border border-primary/30 hover:border-primary/50 transition-all duration-500`}>
                                     <span>{exp.period}</span>
                                     <h3>{exp.role}</h3>
                                     <p>{exp.company}</p>
