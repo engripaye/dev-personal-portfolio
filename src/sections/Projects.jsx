6cf561ddb2d2a41b5ab9de483e12c969af32df58
mac <@b.tunde.ipaye@gmail.com>
2026-06-13 16:53:29 +0100

    feat: react code updated commit by ENGR. IPAYE

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -172,9 +172,10 @@ export const Projects = () => {
                 </div>
 
                 { /* View All CTA */}
-                <div>
+                <div className="text-center mt-12 animate-fade-in anmation-delay-500">
                 <AnimatedBorderButton>
-
+                    View All Projects
+                    <ArrowUpRight className="w-5 h-5" />
                 </AnimatedBorderButton>
                 </div>
             </div>
