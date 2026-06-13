defdc6db5f1247cceda89ccb5c653a2e8bea7ff0
mac <@b.tunde.ipaye@gmail.com>
2026-06-13 16:16:55 +0100

    feat: react code updated commit by ENGR. IPAYE

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -125,6 +125,11 @@ export const Projects = () => {
                                     alt={projects.title}
                                     className="w-full h-full object-cover transition-transform duration-700 group-hover:scale-110"
                                 />
+                                <div
+                                    className="absolute inset-0
+                                    bg-gradient-to-t from-card via-card/50
+                                    to-transparent opacity-60"
+                                />
                             </div>
                         </div>
                     ))}
