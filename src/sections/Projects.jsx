2348d7ea669bf0212423fc6c1274c12b41c06cb6
mac <@b.tunde.ipaye@gmail.com>
2026-06-13 15:52:27 +0100

    feat: react code updated commit by ENGR. IPAYE

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -95,7 +95,7 @@ export const Projects = () => {
             <div className="text-center mx-auto max-w-3xl mb-16">
                 <span className="text-secondary-foreground text-sm font-medium tracking-wider uppercase animate-fade-in">
                     Featured Work</span>
-                <h2 className="text-4xl md:text-5xl font-bold mt-4 mb-6 animate-delay-100 text-foreground">
+                <h2 className="text-4xl md:text-5xl font-bold mt-4 mb-6 animate-delay-100 text-secondary-foreground">
                     Projects that
                     <span className="font-serif italic font-normal text-white">
                         {" "}
