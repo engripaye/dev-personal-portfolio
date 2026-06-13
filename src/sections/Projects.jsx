714bb82ef73a1d6c9882f90b67490ac9f939818b
mac <@b.tunde.ipaye@gmail.com>
2026-06-13 03:25:42 +0100

    feat: react code updated commit by ENGR. IPAYE

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -2,8 +2,11 @@ const projects = [
     {
         title: "Boujuron Fraud Intelligence Platform",
         description: "An AI-powered fraud intelligence platform designed to detect suspicious transactions, " +
-            "identify fraud patterns, assess risk scores, and provide actionable insights for financial institutions and businesses."
-        image:
+            "identify fraud patterns, assess risk scores, and provide actionable insights for financial institutions and businesses.",
+        image: "/projects/project1.png",
+        tags: ["Python", "FastAPI", "Machine Learning" , "React" , "Microservices", "PostgreSQL", "Docker"],
+        link: "https://boujuron.netlify.app/login",
+        github: "https://github.com/Phantore-Sentinel-Technology/boujuron-intelligence",
     }
 ]
 
