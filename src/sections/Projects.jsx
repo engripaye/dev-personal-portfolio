7a6738cac0abfdc1e4121087afd75f8e4bb92ea5
mac <@b.tunde.ipaye@gmail.com>
2026-06-13 03:19:47 +0100

    feat: react code updated commit by ENGR. IPAYE

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,3 +1,12 @@
+const projects = [
+    {
+        title: "Boujuron Fraud Intelligence Platform",
+        description: "An AI-powered fraud intelligence platform designed to detect suspicious transactions, " +
+            "identify fraud patterns, assess risk scores, and provide actionable insights for financial institutions and businesses."
+        image:
+    }
+]
+
 export const Projects = () => {
     return <section></section>
 }
