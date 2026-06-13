a55ed3f314d2bb819f1d150a4cbc10a8dc7bd179
mac <@b.tunde.ipaye@gmail.com>
2026-06-13 16:25:18 +0100

    feat: react code updated commit by ENGR. IPAYE

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -135,7 +135,7 @@ export const Projects = () => {
                                 />
                                 {/* Overlay Links */}
                                 <div className="absolute inset-0 flex items-center justify-center gap-4 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                                    <a href={projects.link} className="p-3 rounded-full glass hover:bg-primary hover:text-primary-foreground">
+                                    <a href={projects.link} className="p-3 rounded-full glass hover:bg-primary hover:text-primary-foreground transition-all">
                                         <ArrowUpRight />
                                     </a>
                                     <a>
