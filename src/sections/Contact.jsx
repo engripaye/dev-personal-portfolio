a82923feb88ff5f525df74205b2fd594bcb8f44d
mac <@b.tunde.ipaye@gmail.com>
2026-06-14 02:16:48 +0100

    feat: contact class code updated commit by ENGR. IPAYE

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -42,6 +42,9 @@ export const Contact = () => {
         setIsLoading(true);
         setSubmitStatus({ type: null, message: "" });
         try{
+            const serviceId = import.meta.env.VITE_EMAILJS_SERVICE_ID;
+            const templateId = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
+            const publicKey = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
 
         }catch (err) {
 
